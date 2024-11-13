4ca1bf3941fb1b7eba0ddc5bae84be0f76389b12
gilsonsilven <gilsonsilven@gmail.com>
2024-11-13 19:50:17 -0300

    updating user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,12 +8,48 @@ export const create = async (user) => {
     })
 }
 
-/// arrumar depois
+
 export const getById = async (id) => {
     const user = await prisma.users.findUnique({
         where: {
-           id_user:1 
+           id_user:id
+        },
+        select: {
+            id_user: true,
+            name: true,
+            emailprim: true,
+            birth_date: true,
+            phone: true,
+            address: true
         }
     })
     return user
+}
+
+
+export const remove = async (id) => {
+    const user = await prisma.users.delete({
+        where: {
+           id_user:id 
+        }
+    })
+    return user
+}
+
+export const update = async (user) => {
+    const result = await prisma.users.update({
+        where: {
+            id_user: user.id
+        },
+        data: user,
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            emailprim: true,
+            phone: true,
+            address: true
+        }
+    })
+    return result
 }
